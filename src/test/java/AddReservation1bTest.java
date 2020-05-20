268dc2ffdb799ac31c37a31c35322a7d7a069b7e
edwardsSask <edwards4398@saskpolytech.ca>
2020-05-20 17:50:16 -0600

    Added AddReservation1bTest.java and added first Test. Test is currently commented out.

---
+++ src/test/java/AddReservation1bTest.java
@@ -0,0 +1,30 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class AddReservation1bTest extends TestCase
+{
+    @Test
+    public void testBasicOverlap()
+    {
+        /*
+        //Set lot id to 0 for test class
+        Lot testLot = new Lot(LotType.non-Serviced,true, 0);
+        Customer testCustomer = new Customer("John Doe",111 A Street", email@email.org, 4443336666,2223334444,5556662222);
+        //GregorianCalendar of Jan 1st, 2021 12:00PM
+        GregorianCalendar testStartDate = new GregorianCalendar(2021,1,1,12,0);
+        //GregorianCalendar of Jan 5th, 2021 12:00PM
+        GregorianCalendar testEndDate = new GregorianCalendar(2021,1,5,12,0);
+
+        assertEquals("Added Reservation", addReservation(0,testStartDate.getTime(), testEndDate.getTime() testCustomer, 2));
+         */
+
+    }
+
+    //Gregorian Calendar Example stored using military time
+    //GregorianCalendar(int year, int month, int date, int hour, int minute)
+    //Use .getTime() to return a date
+}
