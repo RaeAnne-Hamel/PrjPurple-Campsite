b9e592ac97cd045e29178db9415fc4cd66186787
Sarah Radke <radke3219@saskpolytech.ca>
2020-05-21 10:03:34 -0600

    This is a test to see if i can get it working --sarah

---
+++ src/test/java/TestReservation.java
@@ -0,0 +1,8 @@
+public class TestReservation {
+    //this is just a test to see if i can change fiels on git
+    public static void main (String[] args)
+    {
+        System.out.println("Test!");
+    }
+
+}
