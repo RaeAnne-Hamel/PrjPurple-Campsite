12fd6baa3d6aeb75382c2cc2e3eea60399aa5ed5
Sarah Radke <radke3219@saskpolytech.ca>
2020-05-21 11:50:36 -0600

    This is the start of the reservations class --sarah Radke

---
+++ src/main/java/campground_data/Reservation.java
@@ -0,0 +1,55 @@
+package campground_data;
+
+import javax.management.modelmbean.ModelMBeanNotificationBroadcaster;
+import java.util.Date;
+
+public class Reservation {
+    public Guest[] obCustomerList;
+    public int nCustomerCount;
+    public Date obStart;
+    public Date obEnd;
+    public String cabinType;
+    public int ReservationCode;
+    public static int ReservationID = 0; //TODO: find where this comed from
+    public double price;
+
+
+    public Reservation(Guest[] guestList, int NumberOfCustomers, Date obStart, Date EndDate, String cabinType)
+    {
+        this.obCustomerList = guestList;
+        this.nCustomerCount = NumberOfCustomers;
+        this.obStart = obStart;
+        this.obEnd = EndDate;
+        this.cabinType = cabinType;
+        this.ReservationCode = null;  //TODO: find where this comes from in booking leduger
+        this.price = CalculatePrice();
+
+    }
+
+    public String setCustomerNumber(int number)
+    {
+        if(this.cabinType.equals("Regular"))
+        {
+            if(number <= 4)
+            {
+                return "";
+            }
+        }
+        else if(this.cabinType.equals("Group"))
+        {
+            if(number <= 8)
+            {
+                return "";
+            }
+        }
+        else
+        {
+            return "There is already the maximum number of people booked";
+        }
+
+    }
+
+
+
+
+}
