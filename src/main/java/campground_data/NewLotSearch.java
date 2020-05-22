02bd0cd035f5bcf546b467ca822c436483068c6e
Dustin Wiebe <wiebe2314@saskpolytech.ca>
2020-05-22 10:05:49 -0600

    Finished up chooseDate for now, working on getDates - DW

--- src/main/java/campground_data/NewLotSearch.java
+++ src/main/java/campground_data/NewLotSearch.java
@@ -1,32 +1,54 @@
 package campground_data;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 public class NewLotSearch {
 
-    public static Date[] getDates(Enum nType) {
+    public static Date[] getDates(String nType) {
+
+        Date dateStart = new Date();
+
+
+        for (Lot obLot : BookingsLedger.aLot) {
+            if (obLot.getType.equals(nType)) {
+                
+            }
+        }
 
         return null;
     }
 
-    public static void main(String[] args)
-    {
-        chooseDate(2020, 7, 23, 2020, 7, 30);
-    }
-
-    public static ArrayList<Lot> chooseDate(int nStartYear, int nStartMonth, int nStartDay, int nEndYear, int nEndMonth, int nEndDay) {
-
+    /**
+     * chooseDate will take in the type of lot to be reserved, a starting year/month/day, and an ending year/month/day. It will return a list of all lots of that type
+     * available to reserve for that period of dates.
+     * @param nType
+     * @param nStartYear
+     * @param nStartMonth
+     * @param nStartDay
+     * @param nEndYear
+     * @param nEndMonth
+     * @param nEndDay
+     * @return
+     */
+    public static ArrayList<Lot> chooseDate(String nType, int nStartYear, int nStartMonth, int nStartDay, int nEndYear, int nEndMonth, int nEndDay) {
         ArrayList<Lot> obLots = new ArrayList<>();
 
         Date dateStart = new Date(nStartYear, nStartMonth, nStartDay);
         Date dateEnd = new Date(nEndYear, nEndMonth, nEndDay);
+
+        for (Lot obLot : BookingsLedger.aLot)
+        {
+            if (obLot.getType.equals(nType))
+            {
+                if (!checkOverlap(obLot.getLotID(), dateStart, dateEnd))
+                {
+                    obLots.add(obLot);
+                }
+            }
+        }
 
 //        if (dateStart.compareTo(dateEnd) > 0)
 //        {
@@ -44,10 +66,6 @@ public class NewLotSearch {
 //                .limit(lDaysBetween)
 //                .mapToObj(i -> localDateStart.plusDays(i))
 //                .collect(Collectors.toList());
-
-
-
-        Date dateDiff = new Date(dateEnd.getTime() - dateStart.getTime());
 
         return obLots;
     }
