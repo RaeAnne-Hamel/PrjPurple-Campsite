dcc4590d9c1e2c4b1e3a0b1d4586d822de1bf97a
Sarah Radke <radke3219@saskpolytech.ca>
2020-05-22 10:01:35 -0600

    Some edits to the methods, for better readability --sarah Radke

--- src/main/java/campground_data/Reservation.java
+++ src/main/java/campground_data/Reservation.java
@@ -3,24 +3,25 @@ package campground_data;
 import java.util.Date;
 
 public class Reservation {
-    public Guest[] obCustomerList;
-    public int nCustomerCount;
-    public Date obStart;
-    public Date obEnd;
-    public String cabinType;
-    public int ReservationCode;
-    public static int ReservationID = 0;
+    Customer[] obCustomerList;
+    Date obStartDate, obEndDate;
+    String Status = "Active";
+    public static int StaticReservationID = 0;
+    int ReservationID, nCustomerCount;
+    Lot obLot;
+    double price;
     public double price;
 
 
-    public Reservation(Guest[] guestList, int NumberOfCustomers, Date obStart, Date EndDate, String cabinType)
+    public Reservation(Customer[] customers, int nPeople, Date startDate, Date endDate, Lot obLot)
     {
-        this.obCustomerList = guestList;
-        this.nCustomerCount = NumberOfCustomers;
-        this.obStart = obStart;
-        this.obEnd = EndDate;
-        this.cabinType = cabinType;
-        this.ReservationCode = 0;
+        this.obStartDate = startDate;
+        this.obEndDate = endDate;
+        this.obCustomerList = customers;
+        this.nCustomerCount = nPeople;
+
+       /*Set the ID for the specific reservation*/
+        ReservationID = StaticReservationID++;
         this.price = getPrice();
 
     }
@@ -33,96 +34,136 @@ public class Reservation {
      */
     public String setCustomerNumber(int number)
     {
-        if(this.cabinType.equals("Regular"))
+        switch(obLot){
+            case obLot.getLotType() == LotType.Cabin:
+                return  checkSmallType(number);
+                break;
+
+            case obLot.getLotType() == LotType.ServicedIndividual:
+                return  checkSmallType(number);
+                break;
+
+            case obLot.getLotType() == LotType.NonServicedIndividual:
+                return  checkSmallType(number);
+                break;
+
+            case obLot.getLotType() == LotType.ServicedGroup:
+                return  checkGroupType(number);
+                break;
+
+            case obLot.getLotType() == LotType.NonServicedGroup:
+                return  checkGroupType(number);
+                break;
+
+            case obLot.getLotType() == LotType.DeluxeCabin:
+                return  checkGroupType(number);
+                break;
+            default:
+                return "There is already the maximum number of people booked";
+
+    }
+
+
+
+    }
+
+
+    private String checkSmallType(int number)
+    {
+        if(number <= 4)
         {
-            if(number <= 4)
-            {
-                this.nCustomerCount = number;
-                return "";
-            }
-        }
-        else if(this.cabinType.equals("Group"))
-        {
-            if(number <= 8)
-            {
-                this.nCustomerCount = number;
-                return "";
-            }
+            this.nCustomerCount = number;
+            return "";
         }
         else
         {
-            this.nCustomerCount = number;
             return "There is already the maximum number of people booked";
         }
-        return "Somthing has gone wrong";
 
     }
 
-    public double setDiscount(int discount)
-    {
-        if(discount == 100)
+    private String checkGroupType(int number) {
+        if (number <= 8) {
+            this.nCustomerCount = number;
+            return "";
+        } else {
+            return "There is already the maximum number of people booked";
+        }
+    }
+
+
+        public double setDiscount ( int discount)
         {
-            this.price = 0.00;
+            if (discount == 100) {
+                this.price = 0.00;
+                return 0.0;
+            } else if (discount < 100 && discount > 0) {
+                double dCalPrice = this.getPrice() * (discount / 100);
+                dCalPrice = round(dCalPrice, 2);
+                this.price = dCalPrice;
+                return dCalPrice;
+
+            }
+            //if the number is a bad number
+            //we will not change the price of the object.
+            return this.getPrice();
+        }
+
+        /**
+         * this is a method to round the numbers when
+         * calulating the price of items.
+         * @param dVal
+         * @param places
+         * @return
+         */
+        private double round ( double dVal, int places)
+        {
+            long factor = (long) Math.pow(10, places);
+            dVal = dVal * factor;
+            long temp = Math.round(dVal);
+            return (temp / factor);
+
+        }
+
+
+        private double getPrice () {
+            //for  now the price will always be 0
+            //they can maualy input a price if they need.
             return 0.0;
         }
-        else if (discount <100 && discount > 0)
+
+        /**
+         * allows the person to be able to
+         * set a price for the reservation.
+         * @param newPrice
+         */
+        private void setPrice ( double newPrice)
         {
-            double dCalPrice = this.getPrice() * (discount / 100);
-            dCalPrice = round(dCalPrice, 2);
-            this.price = dCalPrice;
-            return dCalPrice;
-
+            this.price = newPrice;
         }
-        //if the number is a bad number
-        //we will not change the price of the object.
-        return this.getPrice();
-    }
 
-    /**
-     * this is a method to round the numbers when
-     * calulating the price of items.
-     * @param dVal
-     * @param places
-     * @return
-     */
-    private double round(double dVal, int places)
-    {
-        long factor = (long) Math.pow(10, places);
-        dVal = dVal * factor;
-        long temp = Math.round(dVal);
-        return (temp / factor);
-
-    }
+        /**
+         *
+         * @param
+         * @return
+         */
+        public boolean setSiteType (LotType NewLot)
+        {
+            Lot TempLot = new Lot(NewLot);
+            Boolean check = checkOverlap(TempLot.getLotID(), this.obStartDate, this.obEndDate);
+            if (check) {
+                this.obLot = TempLot;
+                return true;
+            } else {
+                return false;
+            }
+        }
 
 
-    private double getPrice() {
-        //for  now the price will always be 0
-        //they can maualy input a price if they need.
-        return 0.0;
-    }
-
-    /**
-     * allows the person to be able to
-     * set a price for the reservation.
-     * @param newPrice
-     */
-    private void setPrice(double newPrice)
-    {
-        this.price = newPrice;
-    }
-
-    public boolean setSiteType(String siteType){
-
-        return true;
-    }
-
-
-    public String changeDate(Date obStart, Date obEnd)
-    {
-        return "Need to create";
-    }
-
-
+        public String changeDate (Date obStart, Date obEnd)
+        {
+            return "Need to create";
+        }
 
 
 }
