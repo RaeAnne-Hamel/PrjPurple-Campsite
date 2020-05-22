7c925c5375306fad3d8e843b3e23657f6fea24d0
edwardsSask <edwards4398@saskpolytech.ca>
2020-05-22 14:12:26 -0600

    Added BookingsLedger

---
+++ src/main/java/campground_data/BookingsLedger.java
@@ -0,0 +1,15 @@
+package campground_data;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class BookingsLedger
+{
+    private ArrayList<Lot> aLot = new ArrayList<>();
+    private ArrayList<Customer> aCustomer = new ArrayList<Customer>();
+
+    public void addReservation(int nLotID, Date obStartDate, Date obEndDate, Customer obCustomer, int nPeople)
+    {
+
+    }
+}
