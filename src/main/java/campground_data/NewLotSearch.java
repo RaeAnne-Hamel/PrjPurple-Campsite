590458df3380502fc5c73eb13e90a522eb56d8ea
Dustin Wiebe <wiebe2314@saskpolytech.ca>
2020-05-22 09:05:16 -0600

    Added some search functionality and commented it all out since none of it is necessary - DW

--- src/main/java/campground_data/NewLotSearch.java
+++ src/main/java/campground_data/NewLotSearch.java
@@ -1,14 +1,22 @@
 package campground_data;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class NewLotSearch {
 
+    public static Date[] getDates(Enum nType) {
 
-    public static ArrayList<Lot> chooseDate(int nStartYear, int nStartMonth, int nStartDay, int nEndYear, int nEndMonth, int nEndDay) {
+        return null;
+    }
 
-
+    public static ArrayList<Lot> chooseDate(int nStartYear, int nStartMonth, int nStartDay, int nEndYear, int nEndMonth, int nEndDay) throws Exception {
 
         ArrayList<Lot> obLots = new ArrayList<>();
 
@@ -17,10 +25,25 @@ public class NewLotSearch {
 
         if (dateStart.compareTo(dateEnd) > 0)
         {
-            return null;
+            throw new Exception("Start date must be before end date.");
         }
-        
-        return obLots;
 
+        //BookingsLedger.checkOverlap()
+
+        LocalDate localDateStart = dateStart.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate localDateEnd = dateEnd.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        long lDaysBetween = ChronoUnit.DAYS.between(localDateStart, localDateEnd);
+
+        List<LocalDate> allDates = IntStream.iterate(0, i -> i + 1)
+                .limit(lDaysBetween)
+                .mapToObj(i -> localDateStart.plusDays(i))
+                .collect(Collectors.toList());
+
+
+
+        Date dateDiff = new Date(dateEnd.getTime() - dateStart.getTime());
+
+        return obLots;
     }
 }
