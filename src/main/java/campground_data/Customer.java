26a8de9c0e3c220ed907fad9d6b45cffa2264787
Eryk Burnouf <burnouf0120@saskpolytech.ca>
2020-05-22 18:06:28 -0600

    -Customer Methods

--- src/main/java/campground_data/Customer.java
+++ src/main/java/campground_data/Customer.java
@@ -29,6 +29,48 @@ public class Customer {
 
     }
 
+    public Customer() { }
+
+    public int getCustomerID() { return CustomerID; }
+
+    public String getName() { return sName; }
+
+    public String getAddress() { return sAddress; }
+
+    public String getEmail() { return sEmail; }
+
+    public long getFax() { return nFax; }
+
+    public long getPhone() { return nPhone; }
+
+    public long getSecPhone() { return nSecPhone; }
+
+    public int getVisits() { return nVisits; }
+
+    public boolean isFrequent() { return isFrequent; }
+
+    public int getIdPool() { return idPool; }
+
+    public void setCustomerID(int customerID) { CustomerID = customerID; }
+
+    public void setName(String sName) { this.sName = sName; }
+
+    public void setAddress(String sAddress) { this.sAddress = sAddress; }
+
+    public void setEmail(String sEmail) { this.sEmail = sEmail; }
+
+    public void setFax(long nFax) { this.nFax = nFax; }
+
+    public void setPhone(long nPhone) { this.nPhone = nPhone; }
+
+    public void setSecPhone(long nSecPhone) { this.nSecPhone = nSecPhone; }
+
+    public void setVisits(int nVisits) { this.nVisits = nVisits; }
+
+    public void setFrequent(boolean frequent) { isFrequent = frequent; }
+
+    public void setIdPool(int idPool) { this.idPool = idPool; }
+
 
 
 
