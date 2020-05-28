80b1f5a301c38efb483f29dfa2374c3cc6f99d97
Sarah Radke <radke3219@saskpolytech.ca>
2020-05-28 11:48:08 -0600

    Added in mainGui class to start JavaFX tomorow --sarah Radke

---
+++ src/main/java/campground_ui/MainGui.java
@@ -0,0 +1,15 @@
+package campground_ui;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainGui extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+    }
+
+
+}
