ff42088a31f2b29465d3078611f909bd22ee5662
Dustin Wiebe <wiebe2314@saskpolytech.ca>
2020-05-29 11:45:32 -0600

    Worked on edit customer window - DW

--- src/main/java/campground_ui/EditCustomerWindow.java
+++ src/main/java/campground_ui/EditCustomerWindow.java
@@ -1,27 +1,62 @@
 package campground_ui;
 
+import campground_data.Customer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class EditCustomerWindow extends Application {
+    private Customer obCustomer;
+
+//    public EditCustomerWindow(Customer obCustomer)
+//    {
+//        this.obCustomer = obCustomer;
+//    }
+
+    TextField txtName;
+    TextField txtAddress;
+    TextField txtCountry;
+    TextField txtProvince;
+    TextField txtCity;
+    TextField txtPostal;
+    TextField txtEmail;
+    TextField txtPhone;
+    TextField txtSecPhone;
+    TextField txtFax;
 
 
     @Override
     public void start(Stage stage) throws Exception {
-        TextField txtName = new TextField();
-        TextField txtAddress = new TextField();
-        TextField txtCountry = new TextField();
-        TextField txtProvince = new TextField();
-        TextField txtCity = new TextField();
-        TextField txtPostal = new TextField();
-        TextField txtEmail = new TextField();
-        TextField txtPhone = new TextField();
-        TextField txtSecPhone = new TextField();
-        TextField txtFax = new TextField();
+        obCustomer = new Customer("bob", "215 bob street",
+                "BC", "VA", "S7N4V2", "Canada", "bob@bob.com",
+                1123456789,1037801234, 1033144567, 4, true);
 
+        txtName = new TextField(obCustomer.getName());
+        txtAddress = new TextField(obCustomer.getAddress());
+        txtCountry = new TextField(obCustomer.getCountry());
+        txtProvince = new TextField(obCustomer.getProvince());
+        txtCity = new TextField(obCustomer.getCity());
+        txtPostal = new TextField(obCustomer.getPostal());
+        txtEmail = new TextField(obCustomer.getEmail());
+        txtPhone = new TextField(String.valueOf(obCustomer.getPhone()));
+        txtSecPhone = new TextField(String.valueOf(obCustomer.getSecPhone()));
+        txtFax = new TextField(String.valueOf(obCustomer.getFax()));
+
+        txtName.setEditable(false);
+        txtAddress.setEditable(false);
+        txtCity.setEditable(false);
+        txtCountry.setEditable(false);
+        txtPostal.setEditable(false);
+        txtProvince.setEditable(false);
+        txtEmail.setEditable(false);
+        txtPhone.setEditable(false);
+        txtSecPhone.setEditable(false);
+        txtFax.setEditable(false);
+
+        Label lblID = new Label("ID: ");
+        Label lblGetID = new Label(String.valueOf(obCustomer.getCustomerID()));
         Label lblName = new Label("Name: ");
         Label lblAddress = new Label("Address: ");
         Label lblCountry = new Label("Country:" );
@@ -33,16 +68,45 @@ public class EditCustomerWindow extends Application {
         Label lblSecPhone = new Label("Secondary Phone Number: ");
         Label lblFax = new Label("Fax Number: ");
 
+        BorderPane obBorder = new BorderPane();
+        HBox obBox = new HBox();
         GridPane obGrid = new GridPane();
-        obGrid.setVgap(5);
-        obGrid.setHgap(5);
 
-        obGrid.addColumn(0, lblName, lblAddress);
-        obGrid.addColumn(1, txtName, txtAddress);
+        obBorder.setBottom(obBox);
+        obBorder.setCenter(obGrid);
+
+        obGrid.setVgap(5);
+        obGrid.setHgap(15);
+
+        obGrid.addColumn(0, lblID, lblName, lblAddress, lblCountry, lblProvince, lblCity, lblPostal, lblEmail,
+                lblPhone, lblSecPhone, lblFax);
+        obGrid.addColumn(1,lblGetID, txtName, txtAddress, txtCountry, txtProvince, txtCity, txtPostal, txtEmail,
+                txtPhone, txtSecPhone, txtFax);
+
+        Button btnBack = new Button("Back to Home");
+        Button btnEdit = new Button("Edit");
+        Button btnConfirm = new Button("Confirm");
+
+        btnEdit.setOnAction(e -> Edit());
+
+        obBox.getChildren().addAll(btnBack, btnEdit, btnConfirm);
 
         stage.setTitle("Edit Customer");
-        stage.setScene(new Scene(obGrid, 400, 800));
+        stage.setScene(new Scene(obBorder, 400, 600));
         stage.show();
+    }
+
+    private void Edit(){
+        txtName.setEditable(true);
+        txtAddress.setEditable(true);
+        txtCity.setEditable(true);
+        txtCountry.setEditable(true);
+        txtPostal.setEditable(true);
+        txtProvince.setEditable(true);
+        txtEmail.setEditable(true);
+        txtPhone.setEditable(true);
+        txtSecPhone.setEditable(true);
+        txtFax.setEditable(true);
     }
 
     public static void main(String[] args) {
