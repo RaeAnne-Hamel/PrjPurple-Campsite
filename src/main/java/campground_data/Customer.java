92b735b3a6aff36dcad0dddf8f0b72d3978edd08
Dylan Attwater <attwater@4231@saskpolytech.ca>
2020-06-01 17:54:19 -0600

    Updated Customer to help with AddCustomerWindow-Dylan

--- src/main/java/campground_data/Customer.java
+++ src/main/java/campground_data/Customer.java
@@ -65,7 +65,7 @@ public class Customer {
     }
 
     public Customer() {
-
+//        this.CustomerID = idPool++;
     }
 
     public int getCustomerID() { return CustomerID; }
@@ -233,6 +233,59 @@ public class Customer {
         this.setSecPhone(nSecPhone);
 
         return "Successfully changed";
+    }
+
+    public String addCustomer(String sName, String sLast, String sAddress, String sProvince,
+                                 String sCity, String sPostal, String sCountry, String sEmail,
+                                 long nPhone, long nFax, long nSecPhone) {
+        if (!(sName.length() > 0 && sName.length() <= MAX_LENGTH)) {
+            return "First name must be between 1 and 255 characters.";
+        }
+        if (!(sLast.length() > 0 && sLast.length() <= MAX_LENGTH)) {
+            return "Last name must be between 1 and 255 characters.";
+        }
+        if (!(sAddress.length() > 0 && sAddress.length() <= MAX_LENGTH)){
+            return "Street address must be between 1 and 255 characters.";
+        }
+
+        if (!(sProvince.length() > 0 && sProvince.length()<=MAX_LENGTH)){
+            return "State/province must be between 1 and 255 characters.";
+        }
+        if (!(sCity.length() > 0 && sCity.length()<=MAX_LENGTH)){
+            return "City must be between 1 and 255 characters.";
+        }
+        if (!(sPostal.length() > 2 && sPostal.length()<=10)){
+            return "Postal code must be between 3 and 10 characters";
+        }
+        if (!(sCountry.length() > 0 && sCountry.length()<=MAX_LENGTH)){
+            return "Country must be between 1 and 255 characters.";
+        }
+        if (!(sEmail.length() > 4 && sEmail.length()<=MAX_LENGTH && sEmail.contains("@"))){
+            return "Email must be between 5 and 255 characters and include an @ symbol.";
+        }
+        if (!(nPhone > 1000000000L && nPhone < 9999999999L)){
+            return "Phone number must be 10 digits.";
+        }
+        if (!(nSecPhone > 1000000000L && nSecPhone < 9999999999L) && nSecPhone != 0){
+            return "Secondary phone number must be 10 digits.";
+        }
+        if (!(nFax > 1000000000L && nFax < 9999999999L) && nFax != 0){
+            return "Fax number must be 10 digits.";
+        }
+
+        this.setName(sName);
+        this.setLast(sLast);
+        this.setAddress(sAddress);
+        this.setProvince(sProvince);
+        this.setCity(sCity);
+        this.setPostal(sPostal);
+        this.setCountry(sCountry);
+        this.setEmail(sEmail);
+        this.setPhone(nPhone);
+        this.setFax(nFax);
+        this.setSecPhone(nSecPhone);
+
+        return "Successfully added";
     }
 }
 
