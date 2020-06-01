4e050a73113befe8ae1e6af5184d584295f99ce2
Dylan Attwater <attwater@4231@saskpolytech.ca>
2020-06-01 17:54:48 -0600

    Still working on adding customer-Dylan

--- src/main/java/campground_ui/AddCustomerWindow.java
+++ src/main/java/campground_ui/AddCustomerWindow.java
@@ -2,80 +2,183 @@ package campground_ui;
 
 import campground_data.Customer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+/**
+ * Dylan Attwater
+ * Adding a valid customer to the system
+ */
 public class AddCustomerWindow extends Application {
+    private Customer obCustomer;
 
-//    public AddCustomerWindow(Customer obCustomer)
+//    public EditCustomerWindow(Customer obCustomer)
 //    {
 //        this.obCustomer = obCustomer;
 //    }
 
-    TextField txtName;
-    TextField txtAddress;
-    TextField txtCity;
-    TextField txtProvince;
-    TextField txtCountry;
-    TextField txtPostal;
-    TextField txtFax;
-    TextField txtEmail;
-    TextField txtPhone;
-    TextField txtSecPhone;
+    //Declaring all the variables that'll be used in multiple methods.
+    TextField txtName = new TextField();
+    TextField txtLast = new TextField();
+    TextField txtAddress = new TextField();
+    TextField txtCountry = new TextField();
+    TextField txtProvince = new TextField();
+    TextField txtCity = new TextField();
+    TextField txtPostal = new TextField();
+    TextField txtEmail = new TextField();
+    TextField txtPhone = new TextField();
+    TextField txtPhone2 = new TextField();
+    TextField txtPhone3 = new TextField();
+    TextField txtSecPhone = new TextField();
+    TextField txtSecPhone2 = new TextField();
+    TextField txtSecPhone3 = new TextField();
+    TextField txtFax = new TextField();
+    TextField txtFax2 = new TextField();
+    TextField txtFax3 = new TextField();
+    Button btnConfirm = new Button();
+    GridPane obGrid = new GridPane();
+    Label lblGetID = new Label();
 
     @Override
     public void start(Stage stage) throws Exception {
-        Customer obCustomer = new Customer();
+        obCustomer = new Customer();
 
-//        obCustomer.setName(txtName.getText());
-//        obCustomer.setAddress(txtAddress.getText());
-//        obCustomer.setCity(txtCity.getText());
-//        obCustomer.setProvince(txtProvince.getText());
-//        obCustomer.setCountry(txtCountry.getText());
-//        obCustomer.setPostal(txtPostal.getText());
-//        obCustomer.setEmail(txtEmail.getText());
-//        obCustomer.setFax(Long.parseLong(txtFax.getText()));
-//        obCustomer.setPhone(Long.parseLong(txtPhone.getText()));
-//        obCustomer.setSecPhone(Long.parseLong(txtSecPhone.getText()));
+        //Setting up all the labels. The customer's ID isn't editable, so it uses a label rather than a text field.
+        Label lblID = new Label("ID");
+        lblGetID = new Label(String.valueOf(obCustomer.getCustomerID()));
+        Label lblName = new Label("Name");
+        Label lblLast = new Label("Last Name");
+        Label lblAddress = new Label("Address");
+        Label lblCountry = new Label("Country");
+        Label lblProvince = new Label("Province/State");
+        Label lblCity = new Label("City");
+        Label lblPostal = new Label("Postal Code");
+        Label lblEmail = new Label("Email");
+        Label lblPhone = new Label("Phone Number");
+        Label lblSecPhone = new Label("Secondary Phone");
+        //Secondary Phone is wider than the other labels, so a minimum width is needed to ensure all text is visible.
+        lblSecPhone.setMinWidth(100);
+        Label lblFax = new Label("Fax Number");
 
-        Label lblID = new Label("ID: ");
-        Label lblGetID = new Label(String.valueOf(obCustomer.getCustomerID()));
-        Label lblName = new Label("Name: ");
-        Label lblAddress = new Label("Address: ");
-        Label lblCity = new Label("City: ");
-        Label lblProvince = new Label("Province/State: ");
-        Label lblCountry = new Label("Country:" );
-        Label lblPostal = new Label("Postal Code: ");
-        Label lblEmail = new Label("Email: ");
-        Label lblPhone = new Label("Phone Number: ");
-        Label lblSecPhone = new Label("Secondary Phone Number: ");
-        Label lblFax = new Label("Fax Number: ");
+        //Setting up the GridPane. The labels all go in the first column.
+        obGrid = new GridPane();
+        obGrid.addColumn(0, lblID, lblName, lblLast, lblAddress, lblCountry, lblProvince, lblCity, lblPostal, lblEmail,
+                lblPhone, lblSecPhone, lblFax);
 
+        //Everything is housed in a BorderPane. The HBox goes at the bottom to house the buttons, while the grid goes in the middle.
         BorderPane obBorder = new BorderPane();
         HBox obBox = new HBox();
-        GridPane obGrid = new GridPane();
-
+        HBox obBoxRight = new HBox();
         obBorder.setBottom(obBox);
         obBorder.setCenter(obGrid);
 
-        obGrid.setVgap(5);
-        obGrid.setHgap(15);
+        //Sets up spacing for the grid.
+        obGrid.setVgap(10);
+        obGrid.setHgap(10);
+        obGrid.setAlignment(Pos.CENTER_LEFT);
+        obGrid.setPadding(new Insets(25, 25, 25, 25));
 
-        obGrid.addColumn(0, lblID, lblName, lblAddress, lblCountry, lblProvince, lblCity, lblPostal, lblEmail,
-                lblPhone, lblSecPhone, lblFax);
-        obGrid.addColumn(1,lblGetID, txtName, txtAddress, txtCountry, txtProvince, txtCity, txtPostal, txtEmail,
-                txtPhone, txtSecPhone, txtFax);
+        //Adds the buttons. Confirm starts out disabled until you actually edit some text.
+        Button btnBack = new Button("Back to Home");
+        btnConfirm = new Button("Confirm");
+        btnConfirm.setOnAction(e -> Confirm());
 
-        Button btnBack = new Button("Back");
-        Button btnConfirm = new Button("Confirm");
+        //All items are added to the grid. The non-phone entries are multiple columns wide, since they're larger.
+        obGrid.add(lblGetID, 1, 0);
+        obGrid.add(txtName, 1, 1, 3, 1);
+        obGrid.add(txtLast, 1, 2, 3, 1);
+        obGrid.add(txtAddress, 1, 3, 3, 1);
+        obGrid.add(txtCountry, 1, 4, 3, 1);
+        obGrid.add(txtProvince, 1, 5, 3, 1);
+        obGrid.add(txtCity, 1, 6, 3, 1);
+        obGrid.add(txtPostal, 1, 7, 2, 1);
+        obGrid.add(txtEmail, 1, 8, 3, 1);
+        obGrid.add(txtPhone, 1, 9);
+        obGrid.add(txtSecPhone, 1, 10);
+        obGrid.add(txtFax, 1, 11);
+        obGrid.add(txtPhone2, 2, 9);
+        obGrid.add(txtSecPhone2, 2, 10);
+        obGrid.add(txtFax2, 2, 11);
+        obGrid.add(txtPhone3, 3, 9);
+        obGrid.add(txtSecPhone3, 3, 10);
+        obGrid.add(txtFax3, 3, 11);
 
-        obBox.getChildren().addAll(btnBack, btnConfirm);
+        //Set up spacing and alignment for the buttons. A second HBox is used so that two buttons can be in the bottom right while the other's in the bottom left.
+        obBox.setSpacing(300);
+        obBox.setAlignment(Pos.CENTER);
+        obBoxRight.getChildren().add(btnConfirm);
+        obBoxRight.setSpacing(10);
+        obBoxRight.setAlignment(Pos.BOTTOM_RIGHT);
+        obBox.getChildren().addAll(btnBack, obBoxRight);
 
-        stage.setTitle("Add Customer");
-        stage.setScene(new Scene(obBorder, 400, 600));
+        //Sets up the scene and the stage.
+        stage.setTitle("Edit Customer Information");
+        stage.setScene(new Scene(obBorder, 500, 500));
         stage.show();
+    }
+
+    /**
+     * Method for the confirm button. Will update the customer's information with what it finds in the TextFields, and give a success alert or an error alert
+     * depending on whether the information is correct.
+     */
+    private void Confirm(){
+
+        obCustomer.setName(txtName.getText());
+        obCustomer.setLast(txtLast.getText());
+        obCustomer.setAddress(txtAddress.getText());
+        obCustomer.setCity(txtCity.getText());
+        obCustomer.setProvince(txtProvince.getText());
+        obCustomer.setCountry(txtCountry.getText());
+        obCustomer.setPostal(txtPostal.getText());
+        obCustomer.setEmail(txtEmail.getText());
+        obCustomer.setFax(Long.parseLong(txtFax.getText()));
+        obCustomer.setPhone(Long.parseLong(txtPhone.getText()));
+        obCustomer.setSecPhone(Long.parseLong(txtSecPhone.getText()));
+
+        //Concatenates the phone boxes into one string each.
+        String sPhone = txtPhone.getText() + txtPhone2.getText() + txtPhone3.getText();
+        String sSecPhone = txtSecPhone.getText() + txtSecPhone2.getText() + txtSecPhone3.getText();
+        String sFax = txtFax.getText() + txtFax2.getText() + txtFax3.getText();
+        //Having the fax or secondary phone fields blank will cause errors, so they're set to 0 if they don't exist.
+        if (sFax.equals("")) {
+            sFax = "0";
+        }
+        if (sSecPhone.equals(""))
+        {
+            sSecPhone = "0";
+        }
+        String sVal = "";
+
+        //Try/catch block to prevent non-numbers from being entered in phone/fax fields.
+        try {
+            sVal = obCustomer.addCustomer(obCustomer.getName(), txtLast.getText(), txtAddress.getText(),
+                    txtProvince.getText(), txtCity.getText(), txtPostal.getText(), txtCountry.getText(), txtEmail.getText(),
+                    Long.parseLong(sPhone), Long.parseLong(sFax), Long.parseLong(sSecPhone));
+        }
+        catch (NumberFormatException exp){
+            sVal = "Please enter only numbers in phone and fax fields.";
+        }
+
+        //An alert will pop up denoting success or failure. The error contents come from Customer's UpdateCustomer method.
+        //If successful, all fields will revert to being non-editable.
+        if (sVal.equals("Successfully added")){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success!");
+            alert.setHeaderText(null);
+            alert.setContentText("Customer successfully added.");
+            alert.showAndWait();
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Detected");
+            alert.setHeaderText("Not all fields were correctly entered.");
+            alert.setContentText(sVal);
+            alert.showAndWait();
+        }
     }
 
     public static void main(String[] args) {
