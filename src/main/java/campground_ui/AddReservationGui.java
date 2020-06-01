c883757e2972c0d0785ada69f21f054463c30ca1
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2020-06-01 12:39:53 -0600

    trying to setup the Gui, still something i am not sure about which is creating problems - RH

--- src/main/java/campground_ui/AddReservationGui.java
+++ src/main/java/campground_ui/AddReservationGui.java
@@ -4,6 +4,7 @@ import campground_data.Customer;
 import campground_data.Lot;
 import campground_data.Reservation;
 import javafx.application.Application;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -22,6 +23,7 @@ public class AddReservationGui extends Application {
     public ArrayList<Customer> obCustomer;
     public ArrayList<Reservation> obRes;
     public ArrayList<Lot> obLot;
+    public ArrayList<Customer> custNameList = new ArrayList<>();
 
     //panes
     private GridPane obGrid;
@@ -31,11 +33,64 @@ public class AddReservationGui extends Application {
     private Label lblCustomers, lblArrival, lblDeparture, lblLotID, lblLotType, lblGuest, lblResID, lblResID2;
 
     //Textfield
-    private TextField
+    private TextField txtArrivalYear, txtArrivalDay, txtArrivalMonth, txtDepartDay, txtDepartMonth, txtDepartYear,
+                        txtGuest, txtLotID;
+
+    //comboBoxes
+    private ComboBox<Customer> cboCust1, cboCust2;
+
+    //button
+
 
     @Override
     public void start(Stage primaryStage) {
 
+        //Creating panes
+        obGrid = new GridPane();
+        obBorder = new BorderPane();
 
+        //Creating Labels
+        lblResID = new Label("Reservation Id: ");
+        lblResID2 = new Label("");
+        lblCustomers = new Label("Customer Names: ");
+        lblLotID = new Label("Lot ID:");
+        lblLotType = new Label("");
+        lblArrival = new Label("Arrival Date: ");
+        lblDeparture = new Label("Departure Date: ");
+        lblGuest = new Label("Number of Guests: ");
+
+        //Creating text fields
+        txtLotID = new TextField();
+        txtArrivalYear = new TextField();
+        txtArrivalMonth = new TextField();
+        txtArrivalDay = new TextField();
+        txtDepartYear = new TextField();
+        txtDepartMonth = new TextField();
+        txtDepartDay = new TextField();
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    private ArrayList<Customer> getCustNameList(ArrayList<Customer> obCustomer)
+    {
+        ArrayList<Customer> obRet = new ArrayList<>();
+
+        for(Customer obCust : obCustomer)
+        {
+            obCust = obCustomer.get
+        }
+
+
+        return obRet;
     }
 }
