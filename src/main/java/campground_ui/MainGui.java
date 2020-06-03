c187fc1a0a1d09ebd688b1ad9580584182b1c607
Eryk Burnouf <burnouf0120@saskpolytech.ca>
2020-06-03 16:25:54 -0600

    Minor adjustments to test data

--- src/main/java/campground_ui/MainGui.java
+++ src/main/java/campground_ui/MainGui.java
@@ -37,31 +37,23 @@ public class MainGui extends Application {
     @Override
     public void start(Stage stage) {
 
+        /**
+         * Test data for adding and editing lots. Feel free to edit, remove or use for yourself. Just used to test the data until
+         * saving functionality is added. -EB
+         */
+
         obBookingsLedger = new BookingsLedger();
 
         Lot obLot1 = new Lot(0);
         Lot obLot2 = new Lot(1);
         Lot obLot3 = new Lot(2);
         Lot obLot4 = new Lot(3);
-        Lot obLot5 = new Lot(4);
-        Lot obLot6 = new Lot(5);
-        Lot obLot7 = new Lot(6);
-        Lot obLot8 = new Lot(7);
-        Lot obLot9 = new Lot(8);
-        Lot obLot10 = new Lot(9);
-        Lot obLot11 = new Lot(10);
 
         obBookingsLedger.addAccommodation(obLot1);
         obBookingsLedger.addAccommodation(obLot2);
         obBookingsLedger.addAccommodation(obLot3);
         obBookingsLedger.addAccommodation(obLot4);
-        obBookingsLedger.addAccommodation(obLot5);
-        obBookingsLedger.addAccommodation(obLot6);
-        obBookingsLedger.addAccommodation(obLot7);
-        obBookingsLedger.addAccommodation(obLot8);
-        obBookingsLedger.addAccommodation(obLot9);
-        obBookingsLedger.addAccommodation(obLot10);
-        obBookingsLedger.addAccommodation(obLot11);
+
 
         //Setting up the image to use is the same in each section - DW
         ImageView imgCamp = new ImageView(Camp);
